4237374e54395a33fcb3f0befd52448c9f7f8eb3
LSE <davifranca2000@gmail.com>
2022-09-01 00:31:46 -0400

    inserindo cors

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from routes.user import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="REST API com FastAPI e MongoDB",
     description="API CRUD simples utilizando o mongodb"
 )
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "https://fastapiteste.herokuapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user)
